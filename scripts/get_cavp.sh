22d7273096bdcfa3b3b4e46a20939efcc6f92868
David Leon Gil <coruus@gmail.com>
2014-08-04 19:32:39 -0400

    Added CAVP test vectors.

---
+++ scripts/get_cavp.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+wget -m --no-parent --accept-regex=".*.pdf" http://csrc.nist.gov/groups/STM/cavp/
+wget -m --no-parent --accept-regex=".*.zip" http://csrc.nist.gov/groups/STM/cavp/
